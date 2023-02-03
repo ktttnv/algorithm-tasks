ed9bbf67c6ba178e24886f96151ce53c953d0c7f
Ekaterina Chehranova <ekaterina.tatanova@insystemplus.ru>
2023-02-03 03:03:07 +0300

    Added solution for LeetCode Max Square Shopping Centre task

---
+++ LeetCode/taskXXXX_max_square_shopping centre.js
@@ -0,0 +1,57 @@
+// command to run: node '.\LeetCode\taskXXXX_max_square_shopping centre.js'
+
+function getMaxSquareShoppingCentre(heights) {
+    let max = 0;
+    let buildingsRow = 0;
+
+    const stack = [];
+
+    heights.forEach(curHeight => {
+        while (isNotEmpty(stack) && pick(stack).height > curHeight) {
+            const topStackElement = stack.pop();
+            buildingsRow += topStackElement.row;
+
+            const curSquare = topStackElement.height * buildingsRow;
+            if (curSquare > max) {
+                max = curSquare;
+            }
+        }
+
+        stack.push({
+            height: curHeight,
+            row: buildingsRow + 1,
+        });
+        buildingsRow = 0;
+    });
+
+    while (isNotEmpty(stack)) {
+        const topStackElement = stack.pop();
+        buildingsRow += topStackElement.row;
+
+        const curSquare = topStackElement.height * buildingsRow;
+        if (curSquare > max) {
+            max = curSquare;
+        }
+    }
+
+    return max;
+}
+
+function isNotEmpty(arr) {
+    return arr.length !== 0;
+}
+
+function pick(arr) {
+    return arr[arr.length - 1];
+}
+
+const testCases = [
+    [1, 2, 3], // 4 when we take buildings with heights 2, 3
+    [3, 7, 8, 18, 6, 7, 2], // 30 when we take buildings with heights 7, 8, 18, 6, 7
+    [7, 6, 5, 4, 5, 6, 7, 1, 100], // 100 when we take one building with height 100
+    [7, 6, 5, 4, 5, 6, 7, 2], // 28 when we take buildings with height 7, 6, 5, 4, 5, 6, 7
+];
+
+testCases.forEach(testCase => {
+    console.log('Buildings:', ...testCase, '-> Max square:', getMaxSquareShoppingCentre(testCase));
+});
